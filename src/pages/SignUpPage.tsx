4a857a4ad6eea2a7930ed5c26360b5bcc1f45625
soumen Gupta <soumencse20@gmail.com>
2024-06-20 22:45:45 +0530

    tailwind css added

---
+++ src/pages/SignUpPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SignUpPage = () => {
+  return (
+    <div className='flex text-3xl font-bold underline'>
+        <div>1st box</div>
+        <div>2nd box</div>
+    </div>
+  )
+}
+
+export default SignUpPage
