0bdc344567bae077a3cfcd5cbbdf0285109d7889
soumen Gupta <soumencse20@gmail.com>
2024-06-21 23:36:18 +0530

    added registration page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import SignUpPage from './pages/SignUpPage'
+import SignUpPage from './pages/HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
---
+++ src/pages/MovieSelectionpage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MovieSelectionpage = () => {
+  return (
+    <div>NovieSelectionpage</div>
+  )
+}
+
+export default MovieSelectionpage
