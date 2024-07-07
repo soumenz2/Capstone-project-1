c411f74fd2628cedcc83dce98ca48cc406f1a0d5
soumen Gupta <soumencse20@gmail.com>
2024-07-07 18:51:55 +0530

    jhjghgf

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,44 +7,17 @@ import CountdownTimer from "../component/Countdowntimer";
 import NotesSection from "../component/Notesection";
 import WeatherWidget from "../component/Weatherwidget";
 import { useNavigate } from "react-router-dom";
-import { useSelector} from 'react-redux';
-import { RootState } from '../redux/store';
 
 
 
-interface FormData {
-  name: string;
-  email: string;
-  username: string;
-  mobile:string;
-  agreeTerms: boolean;
-}
-interface selectedItem {
-  id: number;
-  name: string;
- 
-}
+
+
 const HomePage = () => {
-  const [users, setUsers] = useState<FormData>([]);
-  const [category,setcategory]=useState<selectedItem[]>([]);
+ 
   const navigate = useNavigate();
-  const existingData = useSelector((state: RootState) => state.user.user);
-  const selectedCategory = useSelector((state: RootState) => state.user.category);
 
-  useEffect(() => {
-   
-    
-    if (existingData ) {
-     
-      setUsers(existingData);
-      
-    }
-    if (selectedCategory ) {
-      
-      setcategory(selectedCategory);
-      console.log('selected category:', category);
-    }
-  }, []);
+
+
 
   const onBrowse=()=>{
     navigate('/movielistpage')
@@ -56,7 +29,7 @@ const HomePage = () => {
       <div className="w-9/12">
       <div className="flex ">
         <div className="mid:w-152 w-64">
-          <div className="">   <ProfileCard  /></div>
+          <div className="">   <ProfileCard  /> </div>
      
       <WeatherWidget />
     <CountdownTimer />
